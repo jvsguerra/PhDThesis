12cf38416012fb6a5aa206d5fa9a39faa08e2dc4
jvsguerra <jvsguerra@gmail.com>
2023-12-01 18:35:42 -0300

    Update Monitoring and Analytics of KVFinder-web

---
+++ images/clarity-monitoring/world-map.py
@@ -0,0 +1,136 @@
+import pandas as pd
+import plotly.express as px
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Read data
+countries = pd.read_csv("clarity-countries.csv", index_col=0)
+countries = countries.to_dict()["No. of sessions"]
+countries = {k: [v] for k, v in sorted(countries.items(), key=lambda item: item[1])}
+browsers = pd.read_csv("clarity-browsers.csv")
+os = pd.read_csv("clarity-os.csv")
+
+# COLORBLIND PALETTE
+COLORBLIND = [
+    "#377eb8",  # blue
+    "#ff7f00",  # orange
+    "#4daf4a",  # green
+    "#f781bf",  # pink
+    "#a65628",  # brown
+    "#984ea3",  # purple
+    "#999999",  # gray
+    "#e41a1c",  # red
+    "#dede00",  # yellow
+]
+
+# [Plot 1] Chropopleth map for countries
+
+# Prepare data
+raw = pd.read_csv(
+    "https://raw.githubusercontent.com/plotly/datasets/master/2014_world_gdp_with_codes.csv"
+)
+raw.columns = ["country", "GDP", "code"]
+data = pd.DataFrame(countries).T.reset_index()
+data.columns = ["country", "count"]
+df = pd.merge(raw, data, how="left", on="country")
+df["count"] = df["count"].replace(np.nan, 0)
+
+# Plot
+fig = px.choropleth(
+    df,
+    locations="code",
+    color="count",
+    color_continuous_scale=["#FFFFFF", "#999999", "#377eb8"],
+    labels={"count": "Visitors"},
+    range_color=(0, round(df["count"].max())),
+    projection="equirectangular",  # miller
+    width=1200,
+    height=700,
+)
+
+fig.update_layout(
+    coloraxis_colorbar=dict(
+        thicknessmode="pixels",
+        thickness=40,
+        lenmode="pixels",
+        len=520,
+        yanchor="top",
+        y=0.97,
+        ticks="outside",
+        tickwidth=1,
+        ticklen=10,
+        dtick=1,
+        outlinecolor="black",
+        outlinewidth=1,
+    )
+)
+
+fig.update_coloraxes(
+    colorbar_title_font_size=20,
+)
+
+plt.tight_layout()
+fig.write_image("clarity-countries.png", engine="auto")
+
+# [Plot 2] Pie chart for browsers
+
+fig, ax = plt.subplots(figsize=(6, 3), subplot_kw=dict(aspect="equal"))
+
+wedges, texts = ax.pie(
+    browsers["No. of sessions"],
+    colors=COLORBLIND,
+    wedgeprops=dict(width=0.5, edgecolor="black"),
+    startangle=-40,
+)
+
+bbox_props = dict(boxstyle="square, pad=0.3", fc="w", ec="k", lw=0.72)
+kw = dict(arrowprops=dict(arrowstyle="-"), bbox=bbox_props, zorder=0, va="center")
+
+for i, p in enumerate(wedges):
+    ang = (p.theta2 - p.theta1) / 2.0 + p.theta1
+    y = np.sin(np.deg2rad(ang))
+    x = np.cos(np.deg2rad(ang))
+    horizontalalignment = {-1: "right", 1: "left"}[int(np.sign(x))]
+    connectionstyle = "angle,angleA=0,angleB={}".format(ang)
+    kw["arrowprops"].update({"connectionstyle": connectionstyle})
+    ax.annotate(
+        browsers["Browsers"][i] + " (" + browsers["% of sessions"][i] + ")",
+        xy=(x, y),
+        xytext=(1.35 * np.sign(x), 1.4 * y),
+        horizontalalignment=horizontalalignment,
+        **kw
+    )
+
+plt.tight_layout()
+plt.savefig("clarity-browsers.png", dpi=300)
+
+# [Plot 3] Pie chart for Operating systems
+fig, ax = plt.subplots(figsize=(6, 3), subplot_kw=dict(aspect="equal"))
+
+wedges, texts = ax.pie(
+    os["No. of sessions"],
+    colors=COLORBLIND,
+    wedgeprops=dict(width=0.5, edgecolor="black"),
+    startangle=-40,
+)
+
+bbox_props = dict(boxstyle="square, pad=0.3", fc="w", ec="k", lw=0.72)
+kw = dict(arrowprops=dict(arrowstyle="-"), bbox=bbox_props, zorder=0, va="center")
+
+for i, p in enumerate(wedges):
+    ang = (p.theta2 - p.theta1) / 2.0 + p.theta1
+    y = np.sin(np.deg2rad(ang))
+    x = np.cos(np.deg2rad(ang))
+    horizontalalignment = {-1: "right", 1: "left"}[int(np.sign(x))]
+    connectionstyle = "angle,angleA=0,angleB={}".format(ang)
+    kw["arrowprops"].update({"connectionstyle": connectionstyle})
+    ax.annotate(
+        os["Operating systems"][i] + " (" + os["% of sessions"][i] + ")",
+        xy=(x, y),
+        xytext=(1.35 * np.sign(x), 1.4 * y),
+        horizontalalignment=horizontalalignment,
+        **kw
+    )
+
+plt.tight_layout()
+plt.savefig("clarity-os.png", dpi=300)
---
+++ images/kvweb-usage/kvweb-usage.py
@@ -0,0 +1,91 @@
+# %%
+
+# DADOS DE 14/08/2023
+
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Colorblind palette
+COLORBLIND = [
+    "#377eb8",  # blue
+    "#ff7f00",  # orange
+    "#4daf4a",  # green
+    "#f781bf",  # pink
+    "#a65628",  # brown
+    "#984ea3",  # purple
+    "#999999",  # gray
+    "#dede00",  # yellow
+    "#e41a1c",  # red
+]
+
+# Open raw usage data
+with open("kvweb-usage.txt", "r") as f:
+    lines = f.readlines()
+
+# Process
+dates = []
+for line in lines:
+    line = line.split()
+    dates.append(f"%s-%s-2023 %s" % (line[6], line[5], line[7]))
+
+# Convert to DataFrame and export to csv
+data = pd.DataFrame(dates, columns=["Data"])
+data.Data = pd.to_datetime(data.Data)
+data.to_csv("kvweb-usage.csv", index=False, header=False)
+
+# %%
+# Plot KVFinder-web usage over time
+fig, ax = plt.subplots(1, 1, figsize=(12, 9), clear=True, tight_layout=True)
+
+# Histogram
+x1 = data.groupby(data["Data"].dt.month).count()
+rects = ax.bar(
+    x=["Mar-23", "Apr-23", "May-23", "Jun-23", "Jul-23", "Aug-23", "Sep-23", "Oct-23", "Nov-23"],
+    height=x1.Data.values,
+    align="center",
+    ecolor="black",
+    capsize=5,
+    width=0.5,
+    edgecolor="black",
+    color="#377eb8",
+    alpha=0.8,
+)
+
+# Annotate bars
+for rect in rects:
+    height = rect.get_height()
+    ax.text(
+        rect.get_x() + rect.get_width() / 2.0,
+        1.05 * height,
+        "%d" % int(height),
+        ha="center",
+        va="bottom",
+    )
+
+# Cumulative sum
+x2 = data.groupby(data["Data"].dt.month).count().cumsum()
+print(x2)
+ax.plot(
+    ["Mar-23", "Apr-23", "May-23", "Jun-23", "Jul-23", "Aug-23", "Sep-23", "Oct-23", "Nov-23"],
+    x2.Data.values,
+    color="black",
+    label="Cumulative sum",
+    marker="o",
+)
+
+# Customize
+ax.grid(which="major", axis="y", linestyle="--")
+ax.set_ylabel("Jobs", size=20)
+# ax.set_xlabel("Relative Error (%)", size=20)
+ax.set_ylim(0, 1100)
+ax.tick_params(axis="x", labelsize=15)
+ax.tick_params(axis="y", labelsize=15)
+ax.yaxis.set_ticks([0, 50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000, 1050, 1100])
+
+# Legend
+ax.legend(loc="upper left", fontsize=20)
+
+plt.savefig("jobs-executed-per-month.png", dpi=300)
+
+# %%
